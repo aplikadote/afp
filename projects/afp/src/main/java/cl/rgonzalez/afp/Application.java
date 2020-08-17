3cda0f211ba0145bba9ac327428a2290625ef57c
aplikadote <aplikadito@gmail.com>
2020-08-16 22:58:44 -0400

    - actualizando pom para generacion .war en /afp

--- projects/afp/src/main/java/cl/rgonzalez/afp/Application.java
+++ projects/afp/src/main/java/cl/rgonzalez/afp/Application.java
@@ -15,6 +15,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
@@ -24,7 +25,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @EntityScan(basePackages = {"cl.rgonzalez.afp.core.db"})
 @EnableJpaRepositories(basePackages = "cl.rgonzalez.afp.core.db")
 @SpringBootApplication
-public class Application {
+public class Application extends SpringBootServletInitializer {
 
     private static final Logger LOG = LoggerFactory.getLogger(Application.class);
 
